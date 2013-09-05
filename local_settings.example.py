7ec8f31eda10b6c55c9e99daf0bba31844c51aad
Samuele Santi <redshadow@hackzine.org>
2013-09-05 13:13:22 +0200

    Added local_settings.example.py

---
+++ local_settings.example.py
@@ -0,0 +1,8 @@
+# Ardomino settings
+# SETTINGS=/path/to/this_file.py python run.py
+
+#SQLALCHEMY_DATABASE_URI = \
+#    "postgresql+psycopg2://user:pass@localhost/ardomino"
+SQLALCHEMY_DATABASE_URI = "sqlite:////tmp/ardomino.sqlite3"
+
+DEBUG = True
