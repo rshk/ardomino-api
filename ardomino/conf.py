8b4d0eee639d8471adb723064c0b25690e9a2a84
Samuele Santi <redshadow@hackzine.org>
2013-09-11 15:17:38 +0200

    Added tests for configuration file parsing

--- ardomino/conf.py
+++ ardomino/conf.py
@@ -1,75 +1,85 @@
 """
 Utilities to read the Ardomino configuration
+
+Load ardomino-specific settings, from ini files
+contained in ARDOMINO_CONF_DIR.
+
+Path to the directory containing settings files can be
+passed via the ARDOMINO_CONF_DIR application setting
+or the ARDOMINO_CONF_DIR environment variable (which,
+if specified, takes precedence).
+
+Configuration is then converted by creating "groups"
+by splitting section names on ':'.
+
+Example::
+
+    [food:Egg]
+    description = A nice round egg
+
+    [food:Spam]
+    description = Spam Spam Spam Spam Spam!
+
+    [person:JohnDoe]
+    first_name = John
+    last_name = Doe
+
+Becomes::
+
+    {
+        'food': {
+            'Egg': {'description': 'A nice round egg'},
+            'Spam': {'description': 'Spam Spam Spam Spam Spam!'},
+        },
+        'person': {
+            'JohnDoe': {
+                'first_name': 'John',
+                'last_name': 'Doe',
+            }
+        }
+    }
 """
 
 from ConfigParser import RawConfigParser
+import os
 
 
 _parsed_conf_file = None
 
 
 def get_configuration():
-    """
-    Load ardomino-specific settings, from ini files
-    contained in ARDOMINO_CONF_DIR.
+    global _parsed_conf_file
+    if _parsed_conf_file is None:
+        _parsed_conf_file = _get_configuration()
+    return _parsed_conf_file
 
-    Path to the directory containing settings files can be
-    passed via the ARDOMINO_CONF_DIR application setting
-    or the ARDOMINO_CONF_DIR environment variable (which,
-    if specified, takes precedence).
 
-    Configuration is then converted by creating "groups"
-    by splitting section names on ':'.
-
-    Example::
-
-        [food:Egg]
-        description = A nice round egg
-
-        [food:Spam]
-        description = Spam Spam Spam Spam Spam!
-
-        [person:JohnDoe]
-        first_name = John
-        last_name = Doe
-
-    Becomes::
-
-        {
-            'food': {
-                'Egg': {'description': 'A nice round egg'},
-                'Spam': {'description': 'Spam Spam Spam Spam Spam!'},
-            },
-            'person': {
-                'JohnDoe': {
-                    'first_name': 'John',
-                    'last_name': 'Doe',
-                }
-            }
-        }
-
-    """
-
-    if _parsed_conf_file is not None:
-        return _parsed_conf_file
-
+def _get_configuration():
     ARDOMINO_CONF_DIR = os.environ.get('ARDOMINO_CONF_DIR')
     if ARDOMINO_CONF_DIR is None:
         from . import app
         ARDOMINO_CONF_DIR = app.config['ARDOMINO_CONF_DIR']
 
-    conf_parser = RawConfigParser()
-    conf_files = (
-        os.path.join(ARDOMINO_CONF_DIR, f)
-        for f in sorted(
-            f2 for f2 in os.listdir(ARDOMINO_CONF_DIR)
-            if (not filename.startswith('.')) and filename.endswith('.ini')
-        ))
-    conf_parser.read(conf_files)
+    conf_parser = create_conf_parser(ARDOMINO_CONF_DIR)
+    return process_conf_files(conf_parser)
 
-    ## Now, create a [group][name][option] dict
-    ## out of the configuration file.
-    ## Group names are taken from [{type}:{name}] section names.
+
+def create_conf_parser(basedir):
+    conf_parser = RawConfigParser()
+    conf_parser.read(find_configuration_files(basedir))
+    return conf_parser
+
+
+def find_configuration_files(basedir):
+    return (
+        os.path.join(basedir, f)
+        for f in sorted(
+            f2 for f2 in os.listdir(basedir)
+            if (not f2.startswith('.')) and f2.endswith('.ini')
+        ))
+
+
+def process_conf_files(conf_parser):
     conf_obj = {}
     for section in conf_parser.sections():
         section_type, name = section.split(':')
@@ -79,7 +89,4 @@ def get_configuration():
             opt: conf_parser.get(section, opt)
             for opt in conf_parser.options(section)
         }
-
-    ## Cache this for later..
-    _parsed_conf_file = conf_obj
     return conf_obj
--- ardomino/tests/test_configuration.py
+++ ardomino/tests/test_configuration.py
@@ -1,3 +1,115 @@
 """
 Tests for configuration file parsers, ...
 """
+
+from ConfigParser import RawConfigParser
+import io
+import os
+import textwrap
+
+import pytest
+
+from ardomino.conf import (process_conf_files,
+                           find_configuration_files,
+                           create_conf_parser)
+
+
+@pytest.fixture
+def conf_dir(tmpdir):
+    with open(str(tmpdir.join('food.ini')), 'w') as f:
+        f.write(textwrap.dedent("""
+        [food:Egg]
+        taste = Great
+
+        [food:Bacon]
+        taste = Delicious
+
+        [food:Spam]
+        taste = Sublime
+        """))
+    with open(str(tmpdir.join('beverages.ini')), 'w') as f:
+        f.write(textwrap.dedent("""
+        [beverage:Coffee]
+        color = black
+
+        [beverage:Milk]
+        color = white
+
+        [beverage:Tea]
+        color = brown
+        """))
+    with open(str(tmpdir.join('pets.ini')), 'w') as f:
+        f.write(textwrap.dedent("""
+        [pet:Cat]
+        says = Meow
+
+        [pet:Dog]
+        says = Bark
+
+        [pet:Snake]
+        says =
+        """))
+    with open(str(tmpdir.join('not-a-conf-file.txt')), 'w') as f:
+        f.write(textwrap.dedent("""
+        [this-is:not]
+        what = a configuration file!
+        """))
+    return tmpdir
+
+
+def test_create_conf_parser(conf_dir):
+    conf_parser = create_conf_parser(str(conf_dir))
+    assert 'pet:Cat' in conf_parser.sections()
+    assert 'food:Bacon' in conf_parser.sections()
+    assert 'beverage:Coffee' in conf_parser.sections()
+    assert 'this-is:not' not in conf_parser.sections()
+
+
+def test_find_configuration_files(conf_dir):
+    found = find_configuration_files(str(conf_dir))
+    assert sorted(found) == [
+        str(conf_dir.join(n))
+        for n in 'beverages.ini', 'food.ini', 'pets.ini'
+    ]
+
+
+def test_process_conf_files():
+    example_conf = textwrap.dedent("""
+    [food:Egg]
+    description = A nice round egg
+
+    [food:Spam]
+    description = Spam Spam Spam Spam Spam!
+
+    [person:JohnDoe]
+    first_name = John
+    last_name = Doe
+    """)
+    expected_result = {
+        'food': {
+            'Egg': {'description': 'A nice round egg'},
+            'Spam': {'description': 'Spam Spam Spam Spam Spam!'},
+        },
+        'person': {
+            'JohnDoe': {
+                'first_name': 'John',
+                'last_name': 'Doe',
+            }
+        }
+    }
+    conf_parser = RawConfigParser()
+    conf_parser.readfp(io.BytesIO(example_conf))
+    result = process_conf_files(conf_parser)
+    assert result == expected_result
+    pass
+
+
+def test_process_conf_dir(conf_dir):
+    parser = create_conf_parser(str(conf_dir))
+    obj = process_conf_files(parser)
+    assert sorted(obj.keys()) == ['beverage', 'food', 'pet']
+    assert sorted(obj['beverage'].keys()) == ['Coffee', 'Milk', 'Tea']
+    assert sorted(obj['food'].keys()) == ['Bacon', 'Egg', 'Spam']
+    assert sorted(obj['pet'].keys()) == ['Cat', 'Dog', 'Snake']
+    assert obj['food']['Bacon'] == {'taste': 'Delicious'}
+    assert obj['food']['Spam'] == {'taste': 'Sublime'}
